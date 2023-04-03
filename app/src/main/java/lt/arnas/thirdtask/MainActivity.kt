61ec0253f58e828b0f1589198e9cfc269a04a9a1
AKasparavicius <kasparavicius.ar@gmail.com>
2023-04-03 17:14:24 +0300

    Add fun setOnClickButtonListener and doTask01

--- app/src/main/java/lt/arnas/thirdtask/MainActivity.kt
+++ app/src/main/java/lt/arnas/thirdtask/MainActivity.kt
@@ -1,10 +1,12 @@
 package lt.arnas.thirdtask
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import org.w3c.dom.Text
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
 val TAG = "my_tag"
 
@@ -21,7 +23,23 @@ class MainActivity : AppCompatActivity() {
         task02TextView = findViewById(R.id.task02TextView)
         task03TextView = findViewById(R.id.task03TextView)
         clickMeButton = findViewById(R.id.clickMeButton)
+        setOnClickButtonListener()
+    }
 
+    private fun setOnClickButtonListener(){
+        clickMeButton.setOnClickListener {
+            doTask01()
+        }
+    }
+
+    fun doTask01(){
+//        Nuskaitom savaitės dieną iš OS ir atvizduojam rezultatą:
+        val sdf = SimpleDateFormat("EEEE")
+        val d = Date()
+        val dayOfTheWeek: String = sdf.format(d)
+        var today = dayOfTheWeek
+        Log.i(TAG, "today's day: $dayOfTheWeek")
+        task01TextView.text = "Today is $dayOfTheWeek"
     }
 
 }
